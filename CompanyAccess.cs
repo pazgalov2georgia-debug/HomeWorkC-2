37081a8ae8e250ed55ba7c0bfd9788d3e21ba0ce
Pazgalov <Pazgalov@DESKTOP-V51K7QM>
2025-12-14 06:57:00 -0800

    Add project files.

---
+++ CompanyAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkC__2
+{
+    internal class CompanyAccess
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
